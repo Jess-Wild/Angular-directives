922882d864ee2c71dd98f45568ef363833ae7607
Jess-Wild <j.victorion.wcs@gmail.com>
2022-10-26 09:42:32 +0200

    exo de quete

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Raclette party 🧀';
+
+  isThisIngredientVital: boolean = true;
+  isGuestListDisplayed: boolean = true;
+  songList: string[] = ["Alejandro", "Ne me quitte pas", "Le temps est bon"];
+}
